1efacb73823379241770b4597e36928a42211983
Sunanda123-png <sunanda02@gmail.com>
2021-10-13 01:01:00 +0530

    Adding comment

--- src/com/bridgelabz/MyNode.java
+++ src/com/bridgelabz/MyNode.java
@@ -1,23 +1,38 @@
 package com.bridgelabz;
 
+/**
+ * Author:-Sunanda Shil
+ * Date:-12/10/21
+ * @param <T>
+ */
+
 public class MyNode<T> {
     private MyNode next;
     private T data;
+    /**
+     * setter method for making private
+     * @param next
+     */
     public void setNext(MyNode next) {
         this.next = next;
     }
-
     public void setData(T data) {
         this.data = data;
     }
-
+    /**
+     * Getter method for making private
+     * @return
+     */
     public MyNode getNext() {
         return next;
     }
-
     public T getData() {
         return data;
     }
+    /**
+     * Creating Constructor
+     * @param data
+     */
     public MyNode(T data) {
         this.data=null;
         this.next=null;
